dd88d243af939c986aee2045e4b3ed77cb587efd
RuiZhangCHN <zhang1rui4@outlook.com>
2020-12-19 20:13:58 +0800

    Update201219

--- pt_finetune.py
+++ pt_finetune.py
@@ -1,5 +1,8 @@
 import json
 import codecs
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import copy
 
 def read_fspc(datapath):
     texts = []
@@ -50,14 +53,19 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 configuration = BertConfig.from_pretrained('bert-base-chinese', num_labels=5)
 model = BertForSequenceClassification.from_pretrained('bert-base-chinese', config=configuration)
 model.to(device)
-model.train()
+
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+dev_loader = DataLoader(dev_dataset, batch_size=16, shuffle=True)
 
 optim = AdamW(model.parameters(), lr=5e-5)
 
-for epoch in range(3):
-    for batch in train_loader:
+best_model = None
+best_acc = -1
+
+for epoch in range(5):
+    model.train()
+    for batch in tqdm(train_loader):
         optim.zero_grad()
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
@@ -67,15 +75,58 @@ for epoch in range(3):
         loss.backward()
         optim.step()
 
-model.eval()
+    model.eval()
+    preds, targets = [], []
+    for batch in dev_loader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
+        targets.extend(labels.tolist())
+        preds.extend(outputs.tolist())
 
+    dev_accuracy = accuracy_score(y_true=targets, y_pred=preds)
+    dev_precision_macro = precision_score(y_true=targets, y_pred=preds, average='macro')
+    dev_precision_micro = precision_score(y_true=targets, y_pred=preds, average='micro')
+    dev_recall_macro = recall_score(y_true=targets, y_pred=preds, average='macro')
+    dev_recall_micro = recall_score(y_true=targets, y_pred=preds, average='micro')
+
+    print("[Dev Accuracy]:", dev_accuracy)
+    # print("[Dev Macro Precision]:", dev_precision_macro)
+    # print("[Dev Micro Precision]:", dev_precision_micro)
+    # print("[Dev Macro Recall:", dev_recall_macro)
+    # print("[Dev Micro Recall]:", dev_recall_micro)
+
+    if dev_accuracy > best_acc:
+        best_acc = dev_accuracy
+        best_model = copy.deepcopy(model)
+        print("Saving the Best Model!")
+
+
+model.eval()
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+test_preds, test_targets = [], []
 
 for batch in test_loader:
 
     input_ids = batch['input_ids'].to(device)
     attention_mask = batch['attention_mask'].to(device)
     labels = batch['labels'].to(device)
-    outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+    outputs = best_model(input_ids, attention_mask=attention_mask, labels=labels)
     outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
-    print(labels, outputs.tolist())
+    test_preds.extend(outputs.tolist())
+    test_targets.extend(labels.tolist())
+
+test_accuracy = accuracy_score(y_true=test_targets, y_pred=test_preds)
+test_precision_macro = precision_score(y_true=test_targets, y_pred=test_preds, average='macro')
+test_precision_micro = precision_score(y_true=test_targets, y_pred=test_preds, average='micro')
+test_recall_macro = recall_score(y_true=test_targets, y_pred=test_preds, average='macro')
+test_recall_micro = recall_score(y_true=test_targets, y_pred=test_preds, average='micro')
+
+print("[Test Accuracy]:", test_accuracy)
+print("[Test Macro Precision]:", test_precision_macro)
+print("[Test Micro Precision]:", test_precision_micro)
+print("[Test Macro Recall:", test_recall_macro)
+print("[Test Micro Recall]:", test_recall_micro)
