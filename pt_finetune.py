0820aec8120089c34d4bee7be3c090fd76563c6f
RuiZhangCHN <zhang1rui4@outlook.com>
2020-12-20 22:28:54 +0800

    Update201220

--- pt_finetune.py
+++ pt_finetune.py
@@ -39,107 +39,107 @@ train_texts, dev_texts, train_labels, dev_labels = train_test_split(train_texts,
 overall_results = {}
 
 # ========== Bert-Base ==========
-# from transformers import BertTokenizerFast, BertForSequenceClassification, BertConfig
-# tokenizer = BertTokenizerFast.from_pretrained('bert-base-chinese')
-# overall_results['BertBase'] = {}
-#
-# train_encodings = tokenizer(train_texts, truncation=True, padding=True)
-# dev_encodings = tokenizer(dev_texts, truncation=True, padding=True)
-# test_encodings = tokenizer(test_texts, truncation=True, padding=True)
-#
-# train_dataset = FSPCDataset(train_encodings, train_labels)
-# dev_dataset = FSPCDataset(dev_encodings, dev_labels)
-# test_dataset = FSPCDataset(test_encodings, test_labels)
-#
-# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-# configuration = BertConfig.from_pretrained('bert-base-chinese', num_labels=5)
-# model = BertForSequenceClassification.from_pretrained('bert-base-chinese', config=configuration)
-# model.to(device)
-#
-# train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-# dev_loader = DataLoader(dev_dataset, batch_size=16, shuffle=True)
-#
-# optim = AdamW(model.parameters(), lr=5e-5)
-#
-# best_model = None
-# best_acc = -1
-#
-# for epoch in range(5):
-#     model.train()
-#     for batch in tqdm(train_loader):
-#         optim.zero_grad()
-#         input_ids = batch['input_ids'].to(device)
-#         attention_mask = batch['attention_mask'].to(device)
-#         labels = batch['labels'].to(device)
-#         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-#         loss = outputs[0]
-#         loss.backward()
-#         optim.step()
-#
-#     model.eval()
-#     preds, targets = [], []
-#     for batch in dev_loader:
-#         input_ids = batch['input_ids'].to(device)
-#         attention_mask = batch['attention_mask'].to(device)
-#         labels = batch['labels'].to(device)
-#         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-#         outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
-#         targets.extend(labels.tolist())
-#         preds.extend(outputs.tolist())
-#
-#     dev_accuracy = accuracy_score(y_true=targets, y_pred=preds)
-#     dev_precision_macro = precision_score(y_true=targets, y_pred=preds, average='macro')
-#     dev_precision_micro = precision_score(y_true=targets, y_pred=preds, average='micro')
-#     dev_recall_macro = recall_score(y_true=targets, y_pred=preds, average='macro')
-#     dev_recall_micro = recall_score(y_true=targets, y_pred=preds, average='micro')
-#
-#     print("[Dev Accuracy]:", dev_accuracy)
-#     print("[Dev Macro Precision]:", dev_precision_macro)
-#     print("[Dev Micro Precision]:", dev_precision_micro)
-#     print("[Dev Macro Recall:", dev_recall_macro)
-#     print("[Dev Micro Recall]:", dev_recall_micro)
-#
-#     _key = 'dev' + str(epoch)
-#     overall_results['BertBase'][_key] = dev_accuracy
-#
-#     if dev_accuracy > best_acc:
-#         best_acc = dev_accuracy
-#         best_model = copy.deepcopy(model)
-#         print("Saving the Best Model!")
-#
-#
-# model.eval()
-# test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-#
-# test_preds, test_targets = [], []
-#
-# for batch in test_loader:
-#
-#     input_ids = batch['input_ids'].to(device)
-#     attention_mask = batch['attention_mask'].to(device)
-#     labels = batch['labels'].to(device)
-#     outputs = best_model(input_ids, attention_mask=attention_mask, labels=labels)
-#     outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
-#     test_preds.extend(outputs.tolist())
-#     test_targets.extend(labels.tolist())
-#
-# test_accuracy = accuracy_score(y_true=test_targets, y_pred=test_preds)
-# test_precision_macro = precision_score(y_true=test_targets, y_pred=test_preds, average='macro')
-# test_precision_micro = precision_score(y_true=test_targets, y_pred=test_preds, average='micro')
-# test_recall_macro = recall_score(y_true=test_targets, y_pred=test_preds, average='macro')
-# test_recall_micro = recall_score(y_true=test_targets, y_pred=test_preds, average='micro')
-#
-# print("[Test Accuracy]:", test_accuracy)
-# print("[Test Macro Precision]:", test_precision_macro)
-# print("[Test Micro Precision]:", test_precision_micro)
-# print("[Test Macro Recall:", test_recall_macro)
-# print("[Test Micro Recall]:", test_recall_micro)
-#
-# overall_results['BertBase']['test'] = test_accuracy
+from transformers import BertTokenizerFast, BertForSequenceClassification, BertConfig
+tokenizer = BertTokenizerFast.from_pretrained('ckiplab/bert-base-chinese')
+overall_results['BertBase'] = {}
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+dev_encodings = tokenizer(dev_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+train_dataset = FSPCDataset(train_encodings, train_labels)
+dev_dataset = FSPCDataset(dev_encodings, dev_labels)
+test_dataset = FSPCDataset(test_encodings, test_labels)
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+configuration = BertConfig.from_pretrained('bert-base-chinese', num_labels=5)
+bert_model = BertForSequenceClassification.from_pretrained('bert-base-chinese', config=configuration)
+bert_model.to(device)
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+dev_loader = DataLoader(dev_dataset, batch_size=16, shuffle=True)
+
+optim = AdamW(bert_model.parameters(), lr=5e-5)
+
+best_bert_model = None
+best_bert_acc = -1
+
+for epoch in range(5):
+    bert_model.train()
+    for batch in tqdm(train_loader):
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = bert_model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+    bert_model.eval()
+    preds, targets = [], []
+    for batch in dev_loader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = bert_model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
+        targets.extend(labels.tolist())
+        preds.extend(outputs.tolist())
+
+    dev_accuracy = accuracy_score(y_true=targets, y_pred=preds)
+    dev_precision_macro = precision_score(y_true=targets, y_pred=preds, average='macro')
+    dev_precision_micro = precision_score(y_true=targets, y_pred=preds, average='micro')
+    dev_recall_macro = recall_score(y_true=targets, y_pred=preds, average='macro')
+    dev_recall_micro = recall_score(y_true=targets, y_pred=preds, average='micro')
+
+    print("[Dev Accuracy]:", dev_accuracy)
+    print("[Dev Macro Precision]:", dev_precision_macro)
+    print("[Dev Micro Precision]:", dev_precision_micro)
+    print("[Dev Macro Recall:", dev_recall_macro)
+    print("[Dev Micro Recall]:", dev_recall_micro)
+
+    _key = 'dev' + str(epoch)
+    overall_results['BertBase'][_key] = dev_accuracy
+
+    if dev_accuracy > best_bert_acc:
+        best_bert_acc = dev_accuracy
+        best_bert_model = copy.deepcopy(bert_model)
+        print("Saving the Best Model!")
+
+
+bert_model.eval()
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+test_preds, test_targets = [], []
+
+for batch in test_loader:
+
+    input_ids = batch['input_ids'].to(device)
+    attention_mask = batch['attention_mask'].to(device)
+    labels = batch['labels'].to(device)
+    outputs = best_bert_model(input_ids, attention_mask=attention_mask, labels=labels)
+    outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
+    test_preds.extend(outputs.tolist())
+    test_targets.extend(labels.tolist())
+
+test_accuracy = accuracy_score(y_true=test_targets, y_pred=test_preds)
+test_precision_macro = precision_score(y_true=test_targets, y_pred=test_preds, average='macro')
+test_precision_micro = precision_score(y_true=test_targets, y_pred=test_preds, average='micro')
+test_recall_macro = recall_score(y_true=test_targets, y_pred=test_preds, average='macro')
+test_recall_micro = recall_score(y_true=test_targets, y_pred=test_preds, average='micro')
+
+print("[Test Accuracy]:", test_accuracy)
+print("[Test Macro Precision]:", test_precision_macro)
+print("[Test Micro Precision]:", test_precision_micro)
+print("[Test Macro Recall:", test_recall_macro)
+print("[Test Micro Recall]:", test_recall_micro)
+
+overall_results['BertBase']['test'] = test_accuracy
 
 # ========== Albert Base ==========
-from transformers import AlbertTokenizerFast, AlbertForSequenceClassification, AlbertConfig
-tokenizer = AlbertTokenizerFast.from_pretrained('voidful/albert_chinese_base')
+from transformers import BertTokenizerFast, AlbertForSequenceClassification, AlbertConfig
+tokenizer = BertTokenizerFast.from_pretrained('ckiplab/albert-base-chinese')
 overall_results['AlbertBase'] = {}
 
 train_encodings = tokenizer(train_texts, truncation=True, padding=True)
@@ -151,37 +151,37 @@ dev_dataset = FSPCDataset(dev_encodings, dev_labels)
 test_dataset = FSPCDataset(test_encodings, test_labels)
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-configuration = AlbertConfig.from_pretrained('voidful/albert_chinese_base', num_labels=5)
-model = AlbertForSequenceClassification.from_pretrained('voidful/albert_chinese_base', config=configuration)
-model.to(device)
+configuration = AlbertConfig.from_pretrained('ckiplab/albert-base-chinese', num_labels=5)
+albert_model = AlbertForSequenceClassification.from_pretrained('ckiplab/albert-base-chinese', config=configuration)
+albert_model.to(device)
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 dev_loader = DataLoader(dev_dataset, batch_size=16, shuffle=True)
 
-optim = AdamW(model.parameters(), lr=5e-5)
+optim = AdamW(albert_model.parameters(), lr=5e-5)
 
-best_model = None
-best_acc = -1
+best_albert_model = None
+best_albert_acc = -1
 
 for epoch in range(5):
-    model.train()
+    albert_model.train()
     for batch in tqdm(train_loader):
         optim.zero_grad()
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
         labels = batch['labels'].to(device)
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = albert_model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs[0]
         loss.backward()
         optim.step()
 
-    model.eval()
+    albert_model.eval()
     preds, targets = [], []
     for batch in dev_loader:
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
         labels = batch['labels'].to(device)
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = albert_model(input_ids, attention_mask=attention_mask, labels=labels)
         outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
         targets.extend(labels.tolist())
         preds.extend(outputs.tolist())
@@ -201,13 +201,13 @@ for epoch in range(5):
     _key = 'dev' + str(epoch)
     overall_results['AlbertBase'][_key] = dev_accuracy
 
-    if dev_accuracy > best_acc:
-        best_acc = dev_accuracy
-        best_model = copy.deepcopy(model)
+    if dev_accuracy > best_albert_acc:
+        best_albert_acc = dev_accuracy
+        best_albert_model = copy.deepcopy(albert_model)
         print("Saving the Best Model!")
 
 
-model.eval()
+albert_model.eval()
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
 test_preds, test_targets = [], []
@@ -217,7 +217,7 @@ for batch in test_loader:
     input_ids = batch['input_ids'].to(device)
     attention_mask = batch['attention_mask'].to(device)
     labels = batch['labels'].to(device)
-    outputs = best_model(input_ids, attention_mask=attention_mask, labels=labels)
+    outputs = best_albert_model(input_ids, attention_mask=attention_mask, labels=labels)
     outputs = torch.argmax(torch.softmax(outputs.logits, dim=-1), dim=-1)
     test_preds.extend(outputs.tolist())
     test_targets.extend(labels.tolist())
